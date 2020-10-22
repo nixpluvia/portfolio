c83410dffea27258a37138b29e2feb2e7931ede2
nixpluvia <whbear12@gmail.com>
2020-10-22 15:30:56 +0900

    portfolio work - cheongyang

---
+++ public/pf/cheongyang/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./index.css">
+    <script src="./index.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="top-wrap">
+        </div>
+        <div class="gnb-layout">
+            <ul class="gnb"></ul>
+        </div>
+    </header>
+</body>
+</html>
